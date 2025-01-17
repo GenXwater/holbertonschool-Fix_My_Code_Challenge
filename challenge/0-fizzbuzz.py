a1f162b3533939d32b821a66ee24071504b24c5b
GenXwater <louis.genty@outlook.com>
2025-01-17 22:37:56 +0100

    chmod u+x

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -30,7 +30,7 @@ def fizzbuzz(limit):
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Error: Missing number")  # Clearer error message
+        print("Error: Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
